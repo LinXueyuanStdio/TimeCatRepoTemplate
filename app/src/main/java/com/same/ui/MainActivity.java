6119d2d6770da66f98502a5cc1461503bfdb684c
linxueyuanstdio <761516186@qq.com>
2020-12-27 17:13:26 +0800

    init

---
+++ app/src/main/java/com/same/ui/MainActivity.java
@@ -0,0 +1,13 @@
+package com.same.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
---
+++ app/src/main/java/com/same/ui/MyApplication.java
@@ -0,0 +1,27 @@
+package com.same.ui;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Handler;
+
+import androidx.annotation.NonNull;
+
+public class MyApplication extends Application {
+    @SuppressLint("StaticFieldLeak")
+    public static volatile Context applicationContext;
+    public static volatile Handler applicationHandler;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        applicationContext = this;
+        applicationHandler = new Handler(applicationContext.getMainLooper());
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+}
